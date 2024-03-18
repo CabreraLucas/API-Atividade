03a690bc547896932b2fd0f4d549c0a05b2cfa68
CabreraLucas <lucas.cabrera@fatec.sp.gov.br>
2024-03-18 17:46:45 +0000

    Aaa

---
+++ app/src/main/java/application/controller/AlunoController.java
@@ -0,0 +1,21 @@
+package application.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import application.model.Aluno;
+import application.repository.AlunoRepository;
+
+@RestController
+public class AlunoController {
+    @Autowired
+    private AlunoRepository alunoRepo;
+    
+    @PostMapping("/alunos")
+    public Aluno post(RequestBody Aluno aluno){
+        return alunoRepo.save(aluno);
+    }
+
+}
---
+++ app/src/main/java/application/model/Aluno.java
@@ -0,0 +1,35 @@
+package application.model;
+
+
+public class Aluno {
+    private Long id;
+    private String nome;
+    private int idade;
+    private String curso;
+
+public Long getId() {
+    return id;
+}
+public void setId(Long id) {
+    this.id = id;
+}
+public String getNome() {
+    return nome;
+}
+public void setNome(String nome) {
+    this.nome = nome;
+}
+public int getIdade() {
+    return idade;
+}
+public void setIdade(int idade) {
+    this.idade = idade;
+}
+public String getCurso() {
+    return curso;
+}
+public void setCurso(String curso) {
+    this.curso = curso;
+}
+    
+}
